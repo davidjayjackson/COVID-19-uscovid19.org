b84cfa71ef2cd8b662320c3901880876ff5f146d
davidjayjackson <davidjayjackson@gmail.como>
2020-09-19 15:04:04 -0400

    Updated Sept. 19, 2020.

---
+++ all_models_r_wrong.R
@@ -0,0 +1,13 @@
+rm(list=ls())
+## AS of Sept. 3, 2020
+# Deaths: 186,790
+deaths <- 186790
+# Cases: 6,150,016
+cases <- c(6150016)
+# Date of  first confirmed Case: Jan. 21, 2020
+fcase <- as.Date("2020-09-03") - as.Date("2020-01-31")
+cases/216 * 365 # 10,392,388
+##
+# Date of  first confirmed death: Mar. 1, 2020
+as.Date("2020-03-01") - as.Date("2020-09-03")
+deaths/186 * 365 # 366,550
